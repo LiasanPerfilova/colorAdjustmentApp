c5588c3ca52fd802a306c53470dcdd25b4ce43dc
LiaisanPerfilova <perliaisan@gmail.com>
2023-05-21 18:51:28 -0400

    finish project

--- colour/ViewController.swift
+++ colour/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
+    @IBOutlet var redNumberLabel: UILabel!
+    @IBOutlet var greenNumberLabel: UILabel!
+    @IBOutlet var blueNumberLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        redSlider.thumbTintColor = .red
+        greenSlider.thumbTintColor = .green
+        blueSlider.thumbTintColor = .blue
+        
+        colorView.backgroundColor = UIColor(
+            red:CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+            )
+        redNumberLabel.text = String(format: "%.2f", redSlider.value)
+        greenNumberLabel.text = String(format: "%.2f", greenSlider.value)
+        blueNumberLabel.text = String(format: "%.2f" , blueSlider.value)
+        
+        
     }
-
-
+    
+    
+    @IBAction func SliderAction() {
+        redNumberLabel.text = String(format: "%.2f", redSlider.value)
+        greenNumberLabel.text = String(format: "%.2f", greenSlider.value)
+        blueNumberLabel.text = String(format: "%.2f" , blueSlider.value)
+        
+        
+        colorView.backgroundColor = UIColor(
+            red:CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+            )
+    }
+    
+    
+    
+    
 }
 
